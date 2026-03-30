4df285a5e606fa54ef86f5243b0b54a9a4a50db3
Taiyakis <ezerskas.mindaugas@gmail.com>
2026-03-30 20:28:59 +0300

    just in case

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,61 @@
 export const environment = {
     production: false
 }
+
+// interface IConfig {
+//     api: {
+//         staticId: number,
+//         endPoint: string
+//     }
+//     raids: {
+//         hideRaid?: boolean,
+//         name: string,
+//         raidLevelRequirement: number,
+//         values: {
+//             dps: number,
+//             supp: number,
+//             dpsNames: string[],
+//             suppNames: string[]
+//         }[]
+//     }[]
+// }
+
+// export const config: IConfig = {
+//     api: {
+//         staticId: 1,
+//         endPoint: 'https://api.taiyaki.fun'
+//     },
+//     raids: [
+//         {
+//             name: "Serka 💀",
+//             raidLevelRequirement: 1740,
+//             values: [],
+//         },
+//         {
+//             name: "Serka HM",
+//             raidLevelRequirement: 1730,
+//             values: [],
+//         },
+//         {
+//             hideRaid: true,
+//             name: "Serka NM",
+//             raidLevelRequirement: 1710,
+//             values: [],
+//         },
+//         {
+//             name: "Kazeros HM",
+//             raidLevelRequirement: 1730,
+//             values: [],
+//         },
+//         {
+//             name: "Act 4 HM",
+//             raidLevelRequirement: 1720,
+//             values: [],
+//         },
+//         {
+//             name: "Mordum HM",
+//             raidLevelRequirement: 1700,
+//             values: [],
+//         },
+//     ]
+// }
